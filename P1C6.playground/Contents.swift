920ee8a19eb4e68808ab2ec7b3ac6ef08d9b4cc4
nadarm <ccc7497@gmail.com>
2019-12-16 16:32:29 +0900

    for

--- P1C6.playground/Contents.swift
+++ P1C6.playground/Contents.swift
@@ -163,15 +163,45 @@ case .pizza:
 
 
 
+for i in 0...2 {
+    print(i)
+}
+
+for i in 0...5 {
+    if i.isMultiple(of: 2) {
+        print(i)
+        continue
+    }
+    
+    print("홀수")
+}
+
+let helloSwift: String = "Hello Swift!"
+for char in helloSwift {
+    print(char)
+}
+
+var result: Int = 1
+for _ in 1...3 {
+    result *= 10
+}
+print(result)
 
 
+let friends: [String: Int] = ["jay": 10, "joe": 11, "jenny": 12]
+for tuple in friends {
+    print(tuple)
+}
 
+let 주소: [String: String] = ["도": "충청도", "시군구": "청주시 청원구", "동읍면": "율량동"]
+for (키, 값) in 주소 {
+    print("\(키) : \(값)")
+}
 
-
-
-
-
-
+let 지역번호: Set<String> = ["02", "031", "032"]
+for 번호 in 지역번호 {
+    print(번호)
+}
 
 
 
