898aef151f4e95f9682ad6898411eb8eed9489ee
nadarm <ccc7497@gmail.com>
2019-12-18 12:16:13 +0900

    struct

--- C9.playground/Contents.swift
+++ C9.playground/Contents.swift
@@ -2,3 +2,15 @@ import UIKit
 
 
 
+struct BasicInformation {
+    var name: String
+    var age: Int
+}
+
+var nadarmInfo: BasicInformation = BasicInformation(name: "nadarm", age: 100)
+nadarmInfo.age = 101
+nadarmInfo.name = "NADARM"
+
+let sebaInfo: BasicInformation = BasicInformation(name: "seba", age: 99)
+//sebaInfo.age = 100 // error sebaInfo가 let이라 변경 불가
+
