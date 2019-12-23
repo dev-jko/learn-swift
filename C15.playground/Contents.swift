409d115db4dc4fea1d69c2c9b7d0e7982ce224a8
nadarm <ccc7497@gmail.com>
2019-12-23 18:51:33 +0900

    map

---
+++ C15.playground/Contents.swift
@@ -0,0 +1,49 @@
+let numbers: [Int] = [0, 1, 2, 3, 4, 5]
+
+var doubledNumbers: [Int] = [Int]()
+var strings: [String] = [String]()
+
+for number in numbers {
+    doubledNumbers.append(number * 2)
+    strings.append("\(number)")
+}
+print(doubledNumbers)
+print(strings)
+
+doubledNumbers = numbers.map { $0 * 2 }
+strings = numbers.map { "\($0)" }
+print(doubledNumbers)
+print(strings)
+
+
+let evenNumbers: [Int] = [0, 2, 4, 6, 8]
+let oddNumbers: [Int] = [1, 3, 5, 7, 9]
+let multiplyTwo: (Int) -> Int = { $0 * 2 }
+
+let doubledEvenNumber: [Int] = evenNumbers.map(multiplyTwo)
+print(doubledNumbers)
+
+let doubledOddNumber: [Int] = oddNumbers.map(multiplyTwo)
+print(doubledOddNumber)
+
+
+let dictionary: [String: String] = ["a": "A", "b": "B"]
+
+var keys: [String] = dictionary.map { $0.0 }
+let values: [String] = dictionary.map { $0.1 }
+print(keys)
+print(values)
+
+
+var numberSet: Set<Int> = [1, 2, 3, 4, 5, 6]
+let resultSet = numberSet.map { $0 * 2 }
+print(resultSet)
+
+let optionalInt: Int? = 3
+let resultInt: Int? = optionalInt.map { $0 * 2 }
+print(resultInt)
+
+
+let range: CountableClosedRange = 0...3
+let resultRange: [Int] = range.map { $0 * 2 }
+print(resultRange)
