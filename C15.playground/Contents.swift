b4b399837f95fe4d5eaf81c9b20bfacf69129d03
nadarm <ccc7497@gmail.com>
2019-12-24 20:12:28 +0900

    reduce

--- C15.playground/Contents.swift
+++ C15.playground/Contents.swift
@@ -60,3 +60,71 @@ let evenNumbers3: [Int] = mappedNumbers.filter { $0 % 2 == 0 }
 let evenNumbers4: [Int] = numbers.map { $0 + 3 }.filter { $0 % 2 == 0 }
 print(evenNumbers3)
 print(evenNumbers4)
+
+
+
+var sum: Int = numbers.reduce(0) {
+    print("\($0) + \($1)")
+    return $0 + $1
+}
+print(sum)
+
+let subtract: Int = numbers.reduce(0) {
+    print("\($0) - \($1)")
+    return $0 - $1
+}
+print(subtract)
+
+let sumFromThree: Int = numbers.reduce(3) { $0 + $1 }
+print(sumFromThree)
+
+var subtractFromThree: Int = numbers.reduce(3) { $0 - $1 }
+print(subtractFromThree)
+
+let names: [String] = ["Chope", "Jay", "Joker", "Nova"]
+let reducedName: String = names.reduce("yagom's friend : ") { $0 + ", " + $1 }
+print(reducedName)
+
+sum = numbers.reduce(into: 0) { $0 += $1 }
+print(sum)
+
+subtractFromThree = numbers.reduce(into: 3) { $0 -= $1 }
+print(subtractFromThree)
+
+
+var doubledNumbers2: [Int] = [1, 2, 3].reduce(into: [1, 2]) { (result: inout [Int], next: Int) in
+    guard next.isMultiple(of: 2) else {
+        return
+    }
+    
+    result.append(next * 2)
+}
+print(doubledNumbers2)
+
+doubledNumbers2 = [1, 2] + [1, 2, 3].filter { $0.isMultiple(of: 2) }.map { $0 * 2 }
+print(doubledNumbers2)
+
+var upperCasedNames: [String] = names.reduce(into: []) { $0.append($1.uppercased()) }
+print(upperCasedNames)
+
+upperCasedNames = names.map { $0.uppercased() }
+print(upperCasedNames)
+
+
+print(numbers)
+var result: Int = numbers.filter { $0.isMultiple(of: 2) }.map { $0 * 3 }.reduce(0) { $0 + $1 }
+print(result)
+
+result = 0
+for number in numbers {
+    guard number.isMultiple(of: 2) else {
+        continue
+    }
+    
+    result += number * 3
+}
+print(result)
+
+
+
+
