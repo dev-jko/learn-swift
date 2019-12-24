f8f7e46c9a5619f91bff00659b97f73643309455
nadarm <ccc7497@gmail.com>
2019-12-24 20:33:38 +0900

    map, filter, reduce

--- C15.playground/Contents.swift
+++ C15.playground/Contents.swift
@@ -127,4 +127,25 @@ print(result)
 
 
 
+enum Gender {
+    case male, female, unknown
+}
 
+struct Friend {
+    let name: String
+    let gender: Gender
+    let location: String
+    var age: Int
+}
+
+var friends: [Friend] = []
+friends.append(Friend(name: "Yoobato", gender: .male, location: "발리", age: 26))
+friends.append(Friend(name: "JiSoo", gender: .male, location: "시드니", age: 24))
+friends.append(Friend(name: "JuHyun", gender: .male, location: "경기", age: 30))
+friends.append(Friend(name: "JiYoung", gender: .female, location: "서울", age: 22))
+friends.append(Friend(name: "SungHo", gender: .male, location: "충북", age: 20))
+
+var result2: [Friend] = friends.map { Friend(name: $0.name, gender: $0.gender, location: $0.location, age: $0.age + 1) }
+result2 = result2.filter { $0.location != "서울" && $0.age >= 25 }
+let string: String = result2.reduce("서울 제외 25세 이상") { $0 + "\n" + "\($1.name) \($1.location) \($1.age)세" }
+print(string)
