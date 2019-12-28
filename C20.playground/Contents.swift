509a12d70bbce03781a9b470863e4a8aa039e443
nadarm <ccc7497@gmail.com>
2019-12-28 14:39:01 +0900

    protocol inheritance, class only protocol

--- C20.playground/Contents.swift
+++ C20.playground/Contents.swift
@@ -255,3 +255,63 @@ class School2: Named2 {
 }
 
 
+
+protocol Readable {
+    func read()
+}
+
+protocol Writeable {
+    func write()
+}
+
+protocol ReadSpeakable: Readable {
+    func speak()
+}
+
+protocol ReadWriteSpeakable: Readable, Writeable {
+    func speak()
+}
+
+class SomeClass: ReadWriteSpeakable {
+    func read() {
+        print("Read")
+    }
+    
+    func write() {
+        print("Write")
+    }
+    
+    func speak() {
+        print("Speak")
+    }
+}
+
+
+protocol ClassOnlyProtocol: class, Readable, Writeable {
+    
+}
+
+class SomeClass2: ClassOnlyProtocol {
+    func read() {}
+    func write() {}
+}
+
+//struct SomeStruct: ClassOnlyProtocol {} // error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
