74f32ce9a5f0034a37b847d265884aa951ad659f
nadarm <ccc7497@gmail.com>
2019-12-28 13:58:48 +0900

    protocol mutating method

--- C20.playground/Contents.swift
+++ C20.playground/Contents.swift
@@ -135,3 +135,53 @@ Message2.isSendableInstance(yourPhoneMessage)
 Mail2.isSendableInstance(myPhoneMessage)
 Mail2.isSendableInstance(myMail)
 
+
+
+protocol Resettable {
+    mutating func reset()
+}
+
+class Person: Resettable {
+    var name: String?
+    var age: Int?
+    
+    func reset() {
+        self.name = nil
+        self.age = nil
+    }
+}
+
+struct Point: Resettable {
+    var x: Int = 0
+    var y: Int = 0
+    
+    mutating func reset() {
+        self.x = 0
+        self.y = 0
+    }
+}
+
+enum Direction: Resettable {
+    case east, west, south, north, unknown
+    
+    mutating func reset() {
+        self = .unknown
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
