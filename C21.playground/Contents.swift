dd343e60c383e8f36115c09632827272e875c49e
nadarm <ccc7497@gmail.com>
2019-12-28 17:54:46 +0900

    extension initializer

--- C21.playground/Contents.swift
+++ C21.playground/Contents.swift
@@ -100,15 +100,64 @@ print(++myPosition)
 
 
 
+extension String {
+    init(intTypeNumber: Int) {
+        self = "\(intTypeNumber)"
+    }
+    
+    init(doubleTypeNumber: Double) {
+        self = "\(doubleTypeNumber)"
+    }
+}
+
+let stringFromInt: String = String(intTypeNumber: 100)
+let stringFromDouble: String = String(doubleTypeNumber: 100.0)
+
+class Person {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+extension Person {
+    convenience init() {
+        self.init(name: "Unknown")
+    }
+}
+
+let someOne: Person = Person()
+print(someOne.name)
 
 
+struct Size {
+    var width: Double = 0.0
+    var height: Double = 0.0
+}
 
+struct Point {
+    var x: Double = 0.0
+    var y: Double = 0.0
+}
 
+struct Rect {
+    var origin: Point = Point()
+    var size: Size = Size()
+}
 
+let defaultRect: Rect = Rect()
+let memberwiseRect: Rect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
 
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX: Double = center.x - (size.width / 2)
+        let originY: Double = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
 
-
-
+let centerRect: Rect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
 
 
 
