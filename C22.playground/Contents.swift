e96b3bc8ad7dea4b0228f36a1c48e4e3b5a89e50
nadarm <ccc7497@gmail.com>
2019-12-29 22:21:59 +0900

    generic type

--- C22.playground/Contents.swift
+++ C22.playground/Contents.swift
@@ -70,3 +70,78 @@ print("\(numberOne), \(numberTwo)")
 swapTwoValues(&stringOne, &stringTwo)
 print("\(stringOne), \(stringTwo)")
 //swapTwoValues(&numberOne, &stringOne)  // error
+
+
+
+struct IntStack {
+    var items: [Int] = []
+    
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+}
+
+var integerStack: IntStack = IntStack()
+integerStack.push(3)
+print(integerStack.items)
+integerStack.push(2)
+print(integerStack.items)
+integerStack.push(3)
+print(integerStack.items)
+integerStack.push(5)
+print(integerStack.items)
+integerStack.pop()
+print(integerStack.items)
+integerStack.pop()
+print(integerStack.items)
+
+
+struct Stack<Element> {
+    var items: [Element] = []
+    
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    
+    func printStack() {
+        print(self.items)
+    }
+}
+
+var doubleStack: Stack<Double> = Stack()
+doubleStack.push(1)
+doubleStack.push(3.0)
+print(doubleStack.items)
+doubleStack.pop()
+print(doubleStack.items)
+
+var stringStack: Stack<String> = Stack()
+stringStack.push("A")
+print(stringStack.items)
+stringStack.push("B")
+stringStack.printStack()
+
+var anyStack: Stack<Any> = Stack()
+anyStack.push(1)
+anyStack.printStack()
+anyStack.push("A")
+anyStack.printStack()
+anyStack.push(2.0)
+anyStack.printStack()
+
+
+
+
+
+
+
+
+
