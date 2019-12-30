0b8bc2d97ff59c4bae9758619f3427b4261b1079
nadarm <ccc7497@gmail.com>
2019-12-30 20:44:14 +0900

    enumeration case pattern

--- C25.playground/Contents.swift
+++ C25.playground/Contents.swift
@@ -77,6 +77,85 @@ for (x, _) in points {
 
 
 
+if case 0...100 = someValue {
+    print("0 <= \(someValue) <= 100")
+}
+
+let anotherValue: String = "ABC"
+if case "ABC" = anotherValue {
+    print(anotherValue)
+}
+
+enum MainDish {
+    case pasta(taste: String)
+    case pizza(dough: String, topping: String)
+    case chicken(withSauce: Bool)
+    case rice
+}
+
+var dishes: [MainDish] = []
+var dinner: MainDish = .pasta(taste: "크림")
+dishes.append(dinner)
+
+if case .pasta(let taste) = dinner {
+    print("\(taste) 파스타")
+}
+
+dinner = .pizza(dough: "치즈크러스트", topping: "불고기")
+dishes.append(dinner)
+
+func whatIsThis(dish: MainDish) {
+    guard case .pizza(let dough, let topping) = dish else {
+        print("It's not a Pizza")
+        return
+    }
+    
+    print("\(dough) \(topping) 피자")
+}
+whatIsThis(dish: dinner)
+
+dinner = .chicken(withSauce: true)
+dishes.append(dinner)
+
+while case .chicken(let sauced) = dinner {
+    print("\(sauced ? "양념" : "후라이드") 통닭")
+    break
+}
+
+dinner = .rice
+dishes.append(dinner)
+
+if case .rice = dinner {
+    print("오늘 저녁은 밥입니다.")
+}
+
+for dish in dishes {
+    switch dish {
+    case let .pasta(taste):
+        print(taste)
+    case let .pizza(dough, topping):
+        print(dough, topping)
+    case let .chicken(withSauce):
+        print(withSauce)
+    case let .rice:
+        print("Just 쌀")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
