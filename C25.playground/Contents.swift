adc9cdb06815c97a768d5c60f321d2bdb0385bbe
nadarm <ccc7497@gmail.com>
2019-12-30 20:55:47 +0900

    type casting pattern

--- C25.playground/Contents.swift
+++ C25.playground/Contents.swift
@@ -179,7 +179,15 @@ for case let number? in arrayOfOptionalInts {
 
 
 
-
+let someValue2: Any = 100
+switch someValue2 {
+case is String:
+    print("It's String")
+case let value as Int:
+    print(value + 1)
+default:
+    print("Int도 String도 아닙니다.")
+}
 
 
 
