471fab85b3ee179048be5240151969e4f253d60f
nadarm <ccc7497@gmail.com>
2019-12-30 21:37:42 +0900

    optional pattern + where

--- C26.playground/Contents.swift
+++ C26.playground/Contents.swift
@@ -43,3 +43,11 @@ case let total where total < 0:
 case let total:
     print(total)
 }
+
+
+
+let arrayOfOptionalInts: [Int?] = [nil, 2, 3, nil, 5]
+
+for case let number? in arrayOfOptionalInts where number > 2 {
+    print("Found a \(number)")
+}
