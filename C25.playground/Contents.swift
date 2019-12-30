62e477414a91f2b7c7b61d7094da68ea1805f796
nadarm <ccc7497@gmail.com>
2019-12-30 21:23:10 +0900

    expression pattern

--- C25.playground/Contents.swift
+++ C25.playground/Contents.swift
@@ -191,12 +191,125 @@ default:
 
 
 
+switch 3 {
+case 0...5:
+    print("0~5")
+default:
+    print("그 외")
+}
+
+var point: (Int, Int) = (1, 2)
+
+switch point {
+case (0, 0):
+    print("원점")
+case (-2...2, -2...2):
+    print("\(point.0), \(point.1)는 원점과 가까움")
+default:
+    print(point)
+}
+
+func ~= (pattern: String, value: Int) -> Bool {
+    return pattern == "\(value)"
+}
+
+point = (0, 0)
+
+switch point {
+case ("0", "0"):
+    print("원점")
+default:
+    print(point)
+}
+
+struct Person {
+    var name: String
+    var age: Int
+}
+
+let lingo: Person = Person(name: "Lingo", age: 99)
+func ~= (pattern: String, value: Person) -> Bool {
+    return pattern == value.name
+}
+
+func ~= (pattern: Person, value: Person) -> Bool {
+    return pattern.name == value.name && pattern.age == value.age
+}
+
+switch lingo {
+case Person(name: "Lingo", age: 99):
+    print("Same Person")
+case "Lingo":
+    print("Hello Lingo")
+default:
+    print("I don't know")
+}
 
 
+protocol Personalize {
+    var name: String { get }
+    var age: Int { get }
+}
 
+struct Person2: Personalize {
+    var name: String
+    var age: Int
+}
 
+let star: Person2 = Person2(name: "Star", age: 99)
 
+func ~= <T: Personalize>(pattern: String, value: T) -> Bool {
+    return pattern == value.name
+}
 
+func ~= <T: Personalize>(pattern: T, value: T) -> Bool {
+    return pattern.name == value.name && pattern.age == value.age
+}
 
+switch star {
+case Person2(name: "Star", age: 99):
+    print("Same person")
+case "Star":
+    print("Hello Star")
+default:
+    print("I don't know")
+}
 
+func ~= <T: Personalize>(pattern: (T) -> Bool, value: T) -> Bool {
+    return pattern(value)
+}
 
+func young<T: Personalize>(value: T) -> Bool {
+    return value.age < 50
+}
+
+switch star {
+case young:
+    print("young")
+default:
+    print("old")
+}
+
+func isNamed<T: Personalize>(_ pattern: String) -> (T) -> Bool {
+    return { $0.name == pattern }
+}
+
+switch star {
+case isNamed("Jung"):
+    print("He is Jung")
+default:
+    print("Another person")
+}
+
+prefix operator ==?
+
+prefix func ==? <T: Personalize>(pattern: String) -> (T) -> Bool {
+    return isNamed(pattern)
+}
+
+switch star {
+case ==?"Jung":
+    print("He is Jung")
+default:
+    print("Another person")
+}
