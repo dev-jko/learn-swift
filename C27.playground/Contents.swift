736cdb7d10b6b586f198f121eba0b216a3c9e92a
nadarm <ccc7497@gmail.com>
2020-01-01 21:48:07 +0900

    capture list

--- C27.playground/Contents.swift
+++ C27.playground/Contents.swift
@@ -231,3 +231,128 @@ class Person5 {
 var yagom5: Person5? = Person5(name: "yagom5", hobby: "eating")
 print(yagom5?.introduce())
 yagom5 = nil
+
+
+var a = 0
+var b = 0
+let closure = { [a] in
+    print(a, b)
+    b = 20
+}
+
+a = 10
+b = 10
+closure()
+print(b)
+
+
+class SimpleClass {
+    var value: Int = 0
+}
+
+var x = SimpleClass()
+var y = SimpleClass()
+
+let closure2 = { [x] in
+    print(x.value, y.value)
+}
+
+x.value = 10
+y.value = 10
+closure2()
+
+
+var x2: SimpleClass? = SimpleClass()
+y.value = 0
+
+let closure3 = { [weak x2, unowned y] in
+    print(x2?.value, y.value)
+}
+
+x2 = nil
+y.value = 10
+closure3()
+
+
+class Person6 {
+    let name: String
+    let hobby: String?
+    
+    lazy var introduce: () -> String = { [unowned self] in
+        var introduction: String = "My name is \(self.name)"
+        
+        guard let hobby = self.hobby else {
+            return introduction
+        }
+        
+        introduction += " "
+        introduction += "My hobby is \(hobby)"
+        
+        return introduction
+    }
+    
+    init(name: String, hobby: String? = nil) {
+        self.name = name
+        self.hobby = hobby
+    }
+    
+    deinit {
+        print("Person6 \(name) is being deinitialized")
+    }
+}
+
+var yagom6: Person6? = Person6(name: "Yagom", hobby: "eating")
+print(yagom6?.introduce())
+yagom6 = nil
+
+
+yagom6 = Person6(name: "yagom6", hobby: "eating")
+var hana: Person6? = Person6(name: "hana", hobby: "playing guitar")
+
+hana?.introduce = yagom6?.introduce ?? { " " }
+print(yagom6?.introduce())
+print(hana?.introduce())
+
+yagom6 = nil
+//print(hana?.introduce())  // error
+
+
+class Person7 {
+    let name: String
+    let hobby: String?
+    
+    lazy var introduce: () -> String = { [weak self] in
+        guard let `self` = self else {
+            return "원래의 참조 인스턴스가 사라졌습니다"
+        }
+        
+        var introduction: String = "name \(self.name)"
+        
+        guard let hobby = self.hobby else {
+            return introduction
+        }
+        
+        introduction += " "
+        introduction += "hobby \(hobby)"
+        
+        return introduction
+    }
+    
+    init(name: String, hobby: String? = nil) {
+        self.name = name
+        self.hobby = hobby
+    }
+    
+    deinit {
+        print("Person7 \(name) is being deinitialized")
+    }
+}
+
+var yagom7: Person7? = Person7(name: "yagom7", hobby: "eating")
+var hana7: Person7? = Person7(name: "hana7", hobby: "playing guiter")
+
+hana7?.introduce = yagom7?.introduce ?? { " " }
+print(yagom7?.introduce())
+
+yagom7 = nil
+print(hana7?.introduce())
